7c1abfbdca96760829b20e062274701820d2a55f
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2022-11-15 20:26:39 +0200

    add task 1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,26 @@
+import Profile from 'components/Profile/Profile';
+import user from 'data/user';
+// import Statistics from 'components/Statistics/Statistics';
+// import data from 'data/data';
+// import FriendList from 'components/FriendList/FriendList';
+// import friends from 'data/friends';
+// import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+// import transactions from 'data/transactions';
+import css from './App.module.css';
+
+export const App = () => {
+  return (
+    <div className={css.container}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} /> */}
+    </div>
+  );
+};
